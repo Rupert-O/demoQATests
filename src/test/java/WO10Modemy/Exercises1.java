520c1ac1ca71312b67499fde1c5767a660d6f1df
Acer3Robert <robert.qwerty1989@gmail.com>
2025-05-14 20:34:02 +0200

    added argument to options to avoid issue org.openqa.selenium.remote.http.ConnectionFailedException:

--- src/test/java/WO10Modemy/Exercises1.java
+++ src/test/java/WO10Modemy/Exercises1.java
@@ -1,4 +1,20 @@
 package WO10Modemy;
 
+import org.junit.Test;
+
 public class Exercises1 {
+    @Test
+    public void FizzBuzz(){
+        for (int i = 1; i <=50; i++){
+            if (i%5 == 0 && i%3 == 0){
+                System.out.println("FizzBuzz " + i);
+            } else if (i%5 == 0) {
+                System.out.println("Buzz " + i);
+            } else if (i%3 == 0) {
+                System.out.println("Fizz " + i);
+            }else {
+                System.out.println(i);
+            }
+        }
+    }
 }
