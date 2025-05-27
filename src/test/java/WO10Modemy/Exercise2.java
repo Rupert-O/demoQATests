3714a280601bd8015defef1b1fc4f7baab706aaf
Acer3Robert <robert.qwerty1989@gmail.com>
2025-05-27 17:18:36 +0200

    terte

--- src/test/java/WO10Modemy/Exercise2.java
+++ src/test/java/WO10Modemy/Exercise2.java
@@ -2,10 +2,7 @@ package WO10Modemy;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Exercise2 {
     private String title = "Lord of the rings";
@@ -62,8 +59,8 @@ public class Exercise2 {
     }
 }
 
-class testOfGetterAndSetter{
-    @Test
+ class testOfGetterAndSetter{
+
     public static void main(String[] args) {
         Exercise2 testObj = new Exercise2();
         System.out.println("wyciagniety tytuł " + testObj.getTitle());
@@ -122,7 +119,107 @@ class ListExercises{
         parzyste.sort(Collections.reverseOrder());
         System.out.println("lista posortowana: " + parzyste);
     }
-//    public void processListMethod(List<Integer> lista){
-//
-//    }
+
+
+}
+
+class findMax{
+
+    public static Integer findMaks(Integer[] tablica){
+        Integer x = tablica[0];
+        for (int i = 0; i < tablica.length; i++) {
+            if(x < tablica[i]){
+                x = tablica[i];
+                //System.out.println(x);
+            }
+        }
+        return x;
+    }
+
+    public static void main(String[] args){
+        Integer[] tab = {4, 15, -3, 22, 8};
+        System.out.println(findMaks(tab));
+    }
+}
+
+class enumAndSwitch{
+    public enum Day{
+        MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY;
+    }
+    public static void main(String[] args){
+        System.out.println( Day.MONDAY.ordinal());
+        Day dzien = Day.MONDAY;
+        switch (dzien){
+            case MONDAY:
+                System.out.println("poniedzialek");
+                break;
+            case TUESDAY:
+                System.out.println("wtorek");
+                break;
+            default:
+                System.out.println("nic");
+                break;
+        }
+    }
+}
+
+class verifyPassword{
+
+    static boolean isValidPassword(String password){
+
+        boolean conditionLength = false;
+        boolean conditionCapitalLetter = false;
+        boolean atLeastOneDigit = false;
+        boolean validationResult = false;
+
+        //verify length
+        Integer passLenghth = password.length();
+        if(passLenghth >= 8){
+            conditionLength = true;
+        }
+        System.out.println("Pass contains sufficient number of character: " + conditionLength  + " (" + passLenghth + ") ");
+
+        // verify containing capital letters
+        conditionCapitalLetter = password.matches(".*[A-Z].*");
+        System.out.println("Pass contains upperCase letters: " + conditionCapitalLetter);
+
+        //verify containing digits
+        atLeastOneDigit = password.matches(".*\\d.*");
+        System.out.println("Pass contains any digit: " + atLeastOneDigit);
+
+        //check if all conditions are TRUE
+        if(conditionLength && conditionCapitalLetter && atLeastOneDigit){
+            validationResult = true;
+        }
+        return validationResult;
+    }
+
+    public static void main(String[] args){
+        String pass = "Ggsgsgfs5gdgfd";
+        System.out.println("result of password validation: " + isValidPassword(pass));
+
+    }
+}
+class randomExercises{
+
+    public static void main(String[] args){
+        Integer[] arr = {12, 4, 5};
+        for(Integer numer : arr){
+            System.out.println(numer);
+        }
+        Integer x = 0;
+        while(x < 5){
+            System.out.println(x);
+            arr[x] = x;
+            x++;
+        }
+        do{
+            System.out.println(x);
+            x++;
+        }while(x < 5);
+
+    }
+    public String randomExercises(){
+        return "fsdfds";
+    }
 }
---
+++ src/test/java/WO10Modemy/Exercise3.java
@@ -0,0 +1,4 @@
+package WO10Modemy;
+
+public class Exercise3 {
+}
